e5dcbee155725e5e37ccb31bbb9d1769b3b88354
davi38 <davi4alves@gmail.com>
2022-10-12 16:41:38 -0300

    feat: folder & file structure

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,15 @@
-import { Text } from "react-native";
+import { View, SafeArea, FlatList, Text, SafeAreaView } from "react-native";
+import { useState } from "react";
+
+import { COLORS, NFTData } from "../constants";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+    </SafeAreaView>
+  );
 };
 
 export default Home;
