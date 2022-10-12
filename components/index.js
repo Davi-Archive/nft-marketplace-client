00ce120fe96d842c0c541e0e9ca51b2e92fc943d
davi38 <davi4alves@gmail.com>
2022-10-12 19:18:00 -0300

    feat: readMore inplemented on description

--- components/index.js
+++ components/index.js
@@ -4,6 +4,7 @@ import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
 import DetailsDesc from "./DetailsDesc";
 import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
 
 export {
   DetailsDesc,
@@ -13,4 +14,5 @@ export {
   FocusedStatusBar,
   CircleButton,
   RectButton,
+  SubInfo,
 };
